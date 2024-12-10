a0db0f8ceb91c6349c70fb182d4d1ad8ea65cfe4
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-12-11 02:56:39 +0800

    Update look_proverid.sh

--- look_proverid.sh
+++ look_proverid.sh
@@ -1,6 +1,6 @@
 # 
-if [[ $(docker ps -qf name=nexuslab-contain) ]]; then
-    docker exec nexuslab-contain /bin/bash -c 'cat /root/.nexus/prover-id'
-else
+if [[ $(docker ps -qf name=nexuslab-container) ]]; then
     docker exec nexuslab-container /bin/bash -c 'cat /root/.nexus/prover-id'
+else
+    docker exec nexuslab-contain /bin/bash -c 'cat /root/.nexus/prover-id'
 fi
