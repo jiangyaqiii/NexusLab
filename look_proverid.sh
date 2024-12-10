94d9d4ed1745ac73e439cfcdabd86ed2caadb7d3
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-12-11 02:41:34 +0800

    Update look_proverid.sh

--- look_proverid.sh
+++ look_proverid.sh
@@ -1,1 +1,2 @@
-docker exec nexuslab-container /bin/bash -c 'cat /root/.nexus/prover-id'
+# docker exec nexuslab-container /bin/bash -c 'cat /root/.nexus/prover-id'
+docker exec nexuslab-contain /bin/bash -c 'cat /root/.nexus/prover-id'
