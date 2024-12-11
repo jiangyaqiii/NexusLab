74dc514484cffccb8d6071e64439c42077d002e5
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-12-11 19:16:54 +0800

    Update nexuslab_restart.sh

--- nexuslab_restart.sh
+++ nexuslab_restart.sh
@@ -1,5 +1,9 @@
-prover_pid=$(pgrep -f "pro")
-kill -9 $prover_pid
-cd "$NEXUS_HOME"
-echo "$proverId" > "$PROVER_ID_FILE"
+docker exec -d nexuslab-contain /bin/bash -c 'kill -9 $(pgrep -f "pro")'
+docker start nexuslab-contain
+
+docker exec -d nexuslab-contain /bin/bash -e proverId="$proverId" -w /root ubuntu:22.04 /bin/bash -c "cd /root/.nexus && echo '$proverId'>prover-id && ./prover beta.orchestrator.nexus.xyz"
+
+
+cd /root/.nexus
+echo '$proverId'>prover-id
 ./prover beta.orchestrator.nexus.xyz
