26c4367687d705fe3fef98d48615cafc0fdab98e
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-12-11 19:18:55 +0800

    Update nexus_docker_restart.sh

--- nexus_docker_restart.sh
+++ nexus_docker_restart.sh
@@ -1,3 +1,6 @@
 docker exec -d nexuslab-contain /bin/bash -c 'kill -9 $(pgrep -f "pro")'
 sleep 5
 docker start nexuslab-contain
+docker exec -d nexuslab-contain /bin/bash -c 'curl -s https://raw.githubusercontent.com/jiangyaqiii/NexusLab/main/nexuslab_restart.sh |bash'
+
+
