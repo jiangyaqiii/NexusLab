6460b758dbde349f3c34160969dcf90cc840cf93
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2025-02-20 00:58:47 +0800

    Update nexuslab_status.sh

--- nexuslab_status.sh
+++ nexuslab_status.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-if [[ $(docker ps -qf name=nexuslab-contain) ]]; then
+# if [[ $(docker ps -qf name=nexuslab-contain) ]]; then
+#     echo "nexuslab正在运行"
+# else
+#     echo "停止"
+# fi
+
+if tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
     echo "nexuslab正在运行"
 else
     echo "停止"
