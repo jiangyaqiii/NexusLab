10e16fb6c061ab958e7a0806656b8fd51600466b
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2025-02-20 15:49:07 +0800

    Update nexuslab_status.sh

--- nexuslab_status.sh
+++ nexuslab_status.sh
@@ -6,8 +6,15 @@
 #     echo "停止"
 # fi
 
-if tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
-    echo "nexuslab正在运行"
+# if tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
+#     echo "nexuslab正在运行"
+# else
+#     echo "停止"
+# fi
+
+
+if screen -list | grep -q nexus; then
+    echo "运行中"
 else
     echo "停止"
 fi
